d0bddbaf1455f48a30656fc148335d16f7942122
alireza <alireza.alemi76@icloud.com>
2024-02-12 20:58:37 -0700

    created RootNavigator and moved all logic in App.tsx to it.

---
+++ navigators/RootNavigator.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {StatusBar} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Color from '../utilities/colors';
+import {LoginScreen} from '../screens/LoginScreen';
+
+const NativeStack = createNativeStackNavigator();
+
+export function RootNavigator(): React.JSX.Element {
+  return (
+    <NavigationContainer>
+      <StatusBar barStyle="light-content" />
+      <NativeStack.Navigator
+        initialRouteName="Login"
+        screenOptions={{
+          headerStyle: {backgroundColor: Color.yellow},
+          headerTintColor: Color.green,
+          headerBackTitleStyle: {},
+        }}>
+        <NativeStack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{headerShown: false}}
+        />
+      </NativeStack.Navigator>
+    </NavigationContainer>
+  );
+}
